96c8e22eeea1dd6e5b1920fadafbaeca2b8e19df
Michael Christie <Michael Christie>
2014-02-25 09:58:12 +0000

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SampleApp::Application.routes.draw do
 
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
   #get "users/new"
 
   root :to => 'static_pages#home'
@@ -10,8 +10,13 @@ SampleApp::Application.routes.draw do
 
   match '/contact', to: 'static_pages#contact'
   
-  #match '/signup',  to: 'users#new',            via: 'get'
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+
+
+
+
 
   match '/help', to: 'static_pages#help'
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -36,8 +36,28 @@ describe "User pages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
+
+
+
+		describe "after saving the user" do
+			before { click_button submit }
+			#let(:user) { User.find_by(email: 'user@example.com') }
+		    let(:user) { User.find_by_email( 'user@example.com' ) }
+
+
+			it { should have_title(user.name) }
+			it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+			it { should have_link('Sign out') }
 		end
+	end
 	
+        describe "after submission" do
+          before { click_button submit }
+
+          it { should have_title('Sign up') }
+          it { should have_content('error') }
+        end
+
 		describe "signup page" do
 		  before { visit signup_path }
 
