320438d58cece4633f55c887838059d7186c4010
Michael Christie <Michael Christie>
2014-03-02 16:57:18 +0000

    sample app complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,14 +15,16 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
 
   #toggle this if the db complains: Can't mass-assign protected attributes: admin
-  #attr_accessible :admin
+  attr_accessible :admin
 
 
   has_secure_password
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
-  has_many :reverse_relationships, foreign_key: "followed_id", class_name:  "Relationship", dependent: :destroy
+  has_many :reverse_relationships, foreign_key: "followed_id", 
+                                    class_name: "Relationship", 
+                                    dependent: :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
 
   before_save { |user| user.email = email.downcase }
@@ -34,14 +36,8 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
-  def feed 
-    #preliminary
-    Micropost.where("user_id = ?", id)
-  end
-
-
   def following?(other_user)
-    relationshipsfind_by_followed_id(other_user.id).
+    relationships.find_by_followed_id(other_user.id)
   end
 
   def follow!(other_user)
@@ -52,12 +48,14 @@ class User < ActiveRecord::Base
     relationships.find_by_followed_id(other_user.id).destroy
   end
 
-
-
+  def feed 
+    Micropost.from_users_followed_by(self)
+    #preliminary example
+    #Micropost.where("user_id = ?", id)
+  end
 
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64      
     end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ SampleApp::Application.routes.draw do
     end
   end
 
-  resources :sessions,   only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   #get "users/new"
 
   root :to => 'static_pages#home'
